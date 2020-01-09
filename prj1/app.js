61ec88895b23e7269ff236e98660cfce2d254b34
Rahul Sharma <rahulsharma15j@gmail.com>
2020-01-09 16:29:03 +0530

    body-parser

--- prj1/app.js
+++ prj1/app.js
@@ -1,16 +1,23 @@
 const http = require("http");
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("IN the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("IN another middleware");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
