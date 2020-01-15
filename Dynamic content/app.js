f712025449c93f3294cb5e9b8f8cbe04dc9f1ab9
Rahul Sharma <rahulsharma15j@gmail.com>
2020-01-15 16:58:25 +0530

    dynamic data

--- Dynamic content/app.js
+++ Dynamic content/app.js
@@ -1,16 +1,17 @@
 const path = require("path");
-const http = require("http");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
