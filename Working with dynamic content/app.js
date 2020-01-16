0e950dfeaab224e8690739f4c01afb598eecc9f0
Rahul Sharma <rahulsharma15j@gmail.com>
2020-01-16 17:55:48 +0530

    pug

--- Working with dynamic content/app.js
+++ Working with dynamic content/app.js
@@ -5,14 +5,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", "views");
+
 const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const shopData = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
-app.use(shopRoutes);
+app.use(shopData.routes);
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
--- Working with dynamic content/routes/admin.js
+++ Working with dynamic content/routes/admin.js
@@ -22,5 +22,4 @@ router.post("/add-product", (req, res, next) => {
 });
 
 exports.routes = router;
-
 exports.products = products;
