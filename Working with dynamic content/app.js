ef610fec935a4e0605275bc2ef9c86d1c1c86c37
Rahul Sharma <rahulsharma15j@gmail.com>
2020-01-21 00:14:28 +0530

    hbs

--- Working with dynamic content/app.js
+++ Working with dynamic content/app.js
@@ -2,10 +2,19 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHbs({
+    extname: "hbs",
+    defaultLayout: "main-layout",
+    layoutsDir: "views/layouts/"
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -18,7 +27,7 @@ app.use("/admin", adminData.routes);
 app.use(shopData.routes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- Working with dynamic content/routes/shop.js
+++ Working with dynamic content/routes/shop.js
@@ -9,7 +9,15 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+    layout: false
+  });
 });
 
 exports.routes = router;
