371447389d1274e019e86d0719bbe27de3df1191
Rahul Sharma <rahulsharma15j@gmail.com>
2020-01-27 23:32:49 +0530

    Dynamic routes

--- Dynamic routes/controllers/shop.js
+++ Dynamic routes/controllers/shop.js
@@ -10,6 +10,16 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render("shop/index", {
@@ -21,13 +31,22 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", { path: "/cart", pageTitle: "Your cart" });
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart"
+  });
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render("shop/orders", { path: "/orders", pageTitle: "Your orders" });
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders"
+  });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render("shop/checkout", { path: "/checkout", pageTitle: "Checkout" });
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout"
+  });
 };
