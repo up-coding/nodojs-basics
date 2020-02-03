ed0bae1e4f41f9d0d7b4eeff97406a20a6fb125b
Rahul Sharma <rahulsharma15j@gmail.com>
2020-02-04 00:17:53 +0530

    cart seq

--- Dynamic routes/controllers/admin.js
+++ Dynamic routes/controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    description: description,
-    imageUrl: imageUrl
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      description: description,
+      imageUrl: imageUrl
+    })
     .then(result => {
       console.log("Created product");
       res.redirect("/admin/products");
@@ -33,8 +34,11 @@ exports.getEditProduct = (req, res, next) => {
   }
 
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    //Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -70,7 +74,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
+    //Product.findAll()
     .then(products => {
       res.render("admin/products", {
         prods: products,
